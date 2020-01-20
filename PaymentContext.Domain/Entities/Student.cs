b4d0b3614290eecf4ba04b03f85f1225f280bfc2
David <david@maisdados.com.br>
2020-01-20 14:15:22 -0300

    Initial Commit

---
+++ PaymentContext.Domain/Entities/Student.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+
+namespace PaymentContext.Domain.Entities
+{
+    public class Student
+    {
+        public Student(string firstName, string lastName, string document, string email)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Document = document;
+            Email = email;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Document { get; set; }
+        public string Email { get; set; }
+        public string Adress { get; set; }
+        public List<Subscription> Subscriptions { get; set; }
+    }
+}
---
+++ PaymentContext.Tests/Entities/StudentTests.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Entities;
+
+namespace PaymentContext.Tests
+{
+    [TestClass]
+    public class StudentTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var student = new Student("David", "Campos", "156561214545", "teste@gmail.com");
+
+        }
+    }
+}
