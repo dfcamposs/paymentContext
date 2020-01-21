4f4bfb2d816e42cf46ac82f9df4991c984dbe169
David <david@maisdados.com.br>
2020-01-21 14:48:13 -0300

    creating objects types

--- PaymentContext.Tests/Entities/StudentTests.cs
+++ PaymentContext.Tests/Entities/StudentTests.cs
@@ -9,9 +9,6 @@ namespace PaymentContext.Tests
         [TestMethod]
         public void AdicinarAssinatura()
         {
-            var subscription = new Subscription(null);
-            var student = new Student("David", "Campos", "156561214545", "teste@gmail.com");
-            student.AddSubscription(subscription);
         }
     }
 }
