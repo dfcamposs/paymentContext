7d1058f562ddc59d18d1bb6be53913ab74d8dfea
David <david@maisdados.com.br>
2020-01-21 19:01:01 -0300

    Creating tests

--- PaymentContext.Tests/Entities/StudentTests.cs
+++ PaymentContext.Tests/Entities/StudentTests.cs
@@ -1,14 +1,58 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Entities;
+using PaymentContext.Domain.Enums;
+using PaymentContext.Domain.ValueObjects;
+using System;
 
 namespace PaymentContext.Tests
 {
     [TestClass]
     public class StudentTests
     {
-        [TestMethod]
-        public void AdicinarAssinatura()
+        private readonly Student _student;
+        private readonly Subscription _subscription;
+        private readonly Name _name;
+        private readonly Document _document;
+        private readonly Email _email;
+        private readonly Address _address;
+
+        public StudentTests()
         {
-           
+            _name = new Name("Bruce", "Wayne");
+            _document = new Document("34170866045", EDocumentType.CPF);
+            _email = new Email("teste@gmail.com");
+            _address = new Address("Rua 1", "1234", "Barreiro", "Belo Horizonte", "MG", "BR", "30644160");
+
+            _student = new Student(_name, _document, _email);
+            _subscription = new Subscription(null);
+        }
+
+        [TestMethod]
+        public void ShouldReturnErrorWhenHadActiveSubscription()
+        {
+            var payment = new PayPalPayment("12345678", DateTime.Now, DateTime.Now.AddDays(5), 10, 10, "WAYNE CORP", _document, _address, _email);
+
+            _subscription.AddPayment(payment);
+            _student.AddSubscription(_subscription);
+            _student.AddSubscription(_subscription);
+
+            Assert.IsTrue(_student.Invalid);
+        }
+
+        [TestMethod]
+        public void ShouldReturnErrorWhenActiveSubscriptionsHasNoPayment()
+        {
+            _student.AddSubscription(_subscription);
+            Assert.IsTrue(_student.Invalid);
+        }
+        [TestMethod]
+        public void ShouldReturnErrorWhenHadNoActiveSubscription()
+        {
+            var payment = new PayPalPayment("12345678", DateTime.Now, DateTime.Now.AddDays(5), 10, 10, "WAYNE CORP", _document, _address, _email);
+
+            _subscription.AddPayment(payment);
+            _student.AddSubscription(_subscription);
+            Assert.IsTrue(_student.Valid);
         }
     }
 }
